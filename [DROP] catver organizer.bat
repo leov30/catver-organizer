2d45fce7bd06bd38be8568971c4feec5a63fc2cc
leov30 <leo_v30@hotmail.com>
2022-12-30 20:42:52 -0800

    Add files via upload

--- [DROP] catver organizer.bat
+++ [DROP] catver organizer.bat
@@ -1,97 +1,85 @@
 @echo off
-title Universal CATVER Game Organizer  ^| Build: August/04/22
 
-
-REM // path/filenames with exclamation marks are not allowed
-SETLOCAL EnableDelayedExpansion
-REM set "_home=%~0" & set "_batch=%~n0.bat"
-REM set "_home=!_home:\%_batch%=!"
-
-
-REM //folder
-set "_folder=%~n1"
-
-if not exist "catver.ini" (
+if "%~1"=="" (
 	title ERROR
-	echo catver.ini was not found...
+	echo ONLY DRAG AND DROP DATAFILE, OR DATFILE+CATVER.INI
+	echo IF NOT CATVER.INI EXIST ONLY USE THE DATAFILE WITH:
+	echo THE CURRENT ARCADE.DAT AND CATVER.INI FROM https://www.progettosnaps.net
+	echo AND MAMEDIFF.EXE http://www.logiqx.com/Tools/
 	pause&exit
 )
 
-findstr /b /c:"[Category]" "catver.ini" >nul 2>&1
-if %errorlevel%==1 (
-	title ERROR
-	echo THIS IS NOT A VALID MAME CATVER.INI FILE
-	pause&exit
+if "%~x1"==".xml" set "_dat=%~1"&set "_catver=%~2"&set "_file=%~n1"
+if "%~x1"==".dat" set "_dat=%~1"&set "_catver=%~2"&set "_file=%~n1"
+
+if "%_dat%"=="" (
+	if "%~x2"==".xml" set "_dat=%~2"&set "_catver=%~1"&set "_file=%~n2"
+	if "%~x2"==".dat" set "_dat=%~2"&set "_catver=%~1"&set "_file=%~n2"
+)
+if "%_dat%"=="" title ERROR&echo NO DATAFILE FOUND&pause&exit
+
+
+cd /d "%~dp0"
+
+md _temp 2>nul
+
+rem //save last complete build to start script faster
+if not exist "_temp\COMPLETE.tmp" del _temp\*.tmp 2>nul
+
+if exist "_temp\%_file%.tmp" (
+	goto :skip
+)else (
+	del _temp\*.tmp 2>nul
 )
 
+set _src=4
+>nul findstr /l /c:"sourcefile=" "%_dat%"&&set _src=6
 
-if not "%_folder%"=="" (
-	if not exist "%_folder%\" (
-		title ERROR
-		echo NOT A VALID FOLDER
-		pause&exit
+if "%_catver%"=="" (call :use_mamediff)else (call :no_mamediff)
+
+sort /unique _temp\temp.tmp /o _temp\menu.tmp
+del _temp\temp.tmp
+
+
+rem //get bios list
+findstr /l /c:"isbios=""yes""" "%_dat%"
+if %errorlevel% equ 1 (
+	for /f tokens^=4^delims^=^" %%g in ('findstr /l /c:"runnable=""no""" "%_dat%"') do (
+		(echo %%g)>>_temp\bios.tmp
 	)
 	
+)else (
+	for /f tokens^=2^,4^delims^=^" %%g in ('findstr /l /c:"isbios=""yes""" "%_dat%"') do (
+		if "%%g"=="yes" (
+			(echo %%h)>>_temp\bios.tmp
+		)else (
+			(echo %%g)>>_temp\bios.tmp
+		)	
+	)
 )
 
-if not exist _temp (
-	md _temp
-) else (
-	del _temp\cat.txt _temp\found.txt
+
+rem //build paren-clone list
+for /f tokens^=2^,%_src%^delims^=^" %%g in ('findstr /l /c:"cloneof=" "%_dat%"') do (
+	(echo %%g	%%h)>>_temp\cloneof.tmp
 )
 
 
-echo ---------------------------------------------------
-echo *     Generating Catver.ini menu items            *
-echo * This can take a while if catver.ini is large    *
-echo ---------------------------------------------------
+type nul>_temp\COMPLETE.tmp
 
+:skip
+rem // old code -----------------------------
 
-REM //good to extract lines, but won't support single category entries
-findstr /r /c:"^[0-9a-z_]*=.*/" "catver.ini" >_temp\catver.1
-REM ****** line counter ************
-set /a "_total_lines=0" & set /a "_count_lines=0"
-for /f "delims=" %%g in (_temp\catver.1) do set /a "_total_lines+=1"
-
-set "_flag=0"
-title Extracting lines from catver.ini...
-for /f "usebackq tokens=2 delims==" %%g in ("_temp\catver.1") do (
-
-	REM ****** line counter ************	
-	set /a "_count_lines+=1"
-	set /a "_percent=(!_count_lines!*100)/!_total_lines!
-	title Extracting lines: !_count_lines! / !_total_lines! ^( !_percent! %% ^)
-
-	REM if /i "%%g"=="[VerAdded]" goto go_out
-	REM if "!_flag!"=="1" (echo %%h) >>"_temp\cat.txt"
-	REM //start adding lines after [Category] is detected
-	REM if /i "%%g"=="[Category]" set "_flag=1"
-	
-	(echo %%g) >>"_temp\cat.txt"
-)
-
-REM :go_out
-
-if not exist "_temp\cat.txt" (
-	title ERROR
-	echo THIS IS NOT A VALID MAME CATVER.INI FILE
-	pause&exit
-)
-
-call :dups_remover cat.txt
-
-sort _temp\cat.txt /o _temp\temp.1
-del _temp\cat.txt & ren _temp\temp.1 cat.txt
-
+setlocal enabledelayedexpansion
+md output 2>nul
+del _temp\found.tmp 2>nul
 set /a "_lines=0"
-for /f "delims=" %%g in (_temp\cat.txt) do set /a "_lines+=1"
+for /f "delims=" %%g in (_temp\menu.tmp) do set /a "_lines+=1"
 
 :add_lines
-
 set /a "_result=%_lines%%%15"
-
 if not "%_result%"=="0" (
-	(echo *) >>_temp\cat.txt
+	(echo *)>>_temp\menu.tmp
 	set /a "_lines+=1"
 	goto add_lines
 )
@@ -104,10 +92,10 @@ cls
 set /a "_num=0"
 set "_opt=none"
 
-for /f "delims=" %%g in (_temp\cat.txt) do (
-	echo %%g
+for /f "delims=" %%g in (_temp\menu.tmp) do (
+	echo. %%g
 	set /a "_num+=1"	
-	if !_num! EQU 15 (
+	if !_num! equ 15 (
 
 		echo[
 		echo              ** Hit 'Enter' to show more **
@@ -128,183 +116,223 @@ goto loop
 :exit_list
 
 REM //validate selection
-findstr /i /r /c:"=.*%_opt%" "_temp\catver.1" >>_temp\found.txt
-if %errorlevel%==1 (
-	title ERROR
-	echo INVALID SELECTION, NOTHING FOUND
-	timeout 2 >nul
+findstr /ir /c:"=.*%_opt%" _temp\catver.tmp >>_temp\found.tmp
+if %errorlevel% equ 1 (
+	title ERROR&echo INVALID SELECTION, NOTHING FOUND&timeout 2 >nul
 	goto loop
-
 )
+
 
 :go_back
 set "_opt2="
 
 cls
-echo What do you want to do with the files? 
+REM echo What do you want to do with the files? 
 echo ------------- OPTIONS ---------------
-if not "%_folder%"=="" (
-	echo 1. move
-	echo 2. copy
-)
+REM if not "%_folder%"=="" (
+	REM echo 1. move
+	REM echo 2. copy
+REM )
 echo 3. preview current list
 echo 4. make a new list
 echo 5. add another category to list
 echo 6. remove items from current list
 echo 7. Make batch script with the list
-echo 9. cleanup and exit script
+REM echo 9. cleanup and exit script
 echo[
 set /p "_opt2=Enter Option Number: "
 
-if not "%_folder%"=="" (
-	if "%_opt2%"=="1" call :do_folder move & goto go_next
-	if "%_opt2%"=="2" call :do_folder copy & goto go_next
-)
-if "%_opt2%"=="3" (
-	cls
-	echo ************ current list ******************
-	echo[
-	type _temp\found.txt
-	echo[
-	pause & goto go_back
-
-)
-
-if "%_opt2%"=="4" del _temp\found.txt & goto loop
-
+REM if not "%_folder%"=="" (
+	REM if "%_opt2%"=="1" call :do_folder move & goto go_next
+	REM if "%_opt2%"=="2" call :do_folder copy & goto go_next
+REM )
+if "%_opt2%"=="3" goto preview_list
+if "%_opt2%"=="4" del _temp\found.tmp&goto loop
 if "%_opt2%"=="5" goto loop
-
-if "%_opt2%"=="6" (
-	copy /y _temp\found.txt _temp\temp.1
-	
-	:go_back2
-	cls
-	echo ************ current list ******************
-	echo[
-	type _temp\temp.1
-	echo[
-	echo --------- ---Options --------------------
-	echo 1. Save list and go back
-	echo 2. Dissmis and go back
-	echo[
-	set /p "_opt3=Enter category remove or option: " || set "_opt3=2"
-	
-	findstr /i /r /v /c:"=.*!_opt3!" "_temp\temp.1" >_temp\temp.2
-	
-	del _temp\temp.1 & ren _temp\temp.2 temp.1
-	
-	if "!_opt3!"=="1" (
-		del _temp\found.txt & ren _temp\temp.1 found.txt
-		goto go_back
-	)
-	if "!_opt3!"=="2" goto go_back
-	
-	goto go_back2
-	
-)
-REM //make batch scirpt
-if "%_opt2%"=="7" call :make_batch & goto go_next
-
-if "%_opt2%"=="9" goto go_next
+if "%_opt2%"=="6" goto edit_list
+if "%_opt2%"=="7" goto make_batch
+REM if "%_opt2%"=="9" goto go_next
 
 echo INVALID OPTION
 timeout 2 >nul
 goto go_back
 
 
-:go_next
+REM :go_next
 
 
 
-del _temp\found.txt _temp\cat.txt _temp\catver.1
-rd _temp
-title FINISHED
-pause&exit
+REM del _temp\found.txt _temp\cat.txt _temp\catver.1
+REM rd _temp
+REM title FINISHED
+REM pause&exit
 
 REM // ------------ end of script --------------------
 
 
-:do_folder
-
-if not exist catver_ORGANIZER md catver_ORGANIZER
-if exist NOTFOUND.txt del NOTFOUND.txt
-
-for /f "tokens=1,2 delims==" %%g in (_temp\found.txt) do (
-	%1 "%_folder%\%%g.zip" catver_ORGANIZER\ >nul 2>&1
-	if "!errorlevel!"=="1" (echo %%g.zip) >>NOTFOUND.txt
-
-)
-
-cls
-echo              **** ALL DONE ***
-echo ------------------------------------------------------
-echo * extracted files are in  the catver_ORGANIZER folder*
-echo *                                                    *
-echo *                                                    *
-echo ------------------------------------------------------
-echo[
-
-exit /b
 
 :make_batch
-(echo @echo off
-echo if "%%~1"=="" echo DRAG AND DROP FOLDER TO THIS SCRIPT ^&pause^&exit
-echo if not exist "%%~1\" echo INVALID FOLDER ^&pause^&exit
-echo if not exist _CATVER md _CATVER
-echo title GAME ORGANIZER ^^^| Catver.ini ^^^| Build: %DATE%
-echo echo This script will create the _CATVER folder, then
-echo echo       MOVE/COPY the matching .zip files
-echo echo ----------------------------------------------
-echo echo.
-echo echo Do you want to COPY or MOVE the files?
-echo echo 1. MOVE
-echo echo 2. COPY
-echo set /p "_opt=Enter Option number: " ^|^| set _opt=1
-echo if "%%_opt%%"=="1" ^(set _opt=MOVE^) else ^(set _opt=COPY^)) >catver_ORGANIZER.bat
 
-for /f "tokens=1,2 delims==" %%g in (_temp\found.txt) do (
-	(echo %%_opt%% "%%~1\%%g.zip" .\_CATVER ^|^| ^(echo %%g.zip^) ^>^>NOTFOUND.txt) >>catver_ORGANIZER.bat
+echo. 
+echo 1. MOVE games ^(use the script to remove unwanted games^)
+echo 2. COPY games ^(use the script to make a playable pack^)
+echo.
+choice /n /c:12 /m "Enter Option: "
+if %errorlevel% equ 1 (
+	rem // add all clones of matched parents, since clones cant run without parent
+	set _opt=move
+	for /f "delims==" %%g in (_temp\found.tmp) do (
+		for /f "delims=	" %%h in ('findstr /el /c:"	%%g" _temp\cloneof.tmp') do (
+			findstr /bl /c:"%%h=" _temp\found.tmp||(echo %%h=Added Clone)>>_temp\found.tmp
+		)
+	)
+)else (
+	rem //add parents of orphan clones, to make matched clones playable
+	rem //add bios too??
+	set _opt=copy
+	call :build_romof
+	for /f "delims==" %%g in (_temp\found.tmp) do (
+		
+		rem //get bios
+		for /f "tokens=2 delims=	" %%h in ('findstr /bl /c:"%%g	" _temp\romof.tmp') do (
+			findstr /bl /c:"%%h=" _temp\found.tmp||(echo %%h=BIOS)>>_temp\found.tmp
+		)
+		rem //add parent
+		for /f "tokens=2 delims=	" %%h in ('findstr /bl /c:"%%g	" _temp\cloneof.tmp') do (
+			findstr /bl /c:"%%h=" _temp\found.tmp||(echo %%h=Added Parent)>>_temp\found.tmp
+		)
+	)
+)
+
+(echo @echo off
+echo title Datafile2script Files Renamer ^^^| "%_file%" ^^^| Build: %DATE%
+
+echo del _NOTFOUND.csv 2^>nul
+echo choice /m "Continue? "
+echo if %%errorlevel%% equ 2 exit
+echo md _CATVER 2^>nul
+echo cls^&echo ... ...)>"output\%_file%.bat"
+
+for /f "tokens=1,2 delims==" %%g in (_temp\found.tmp) do (
+	(echo %_opt% /y %%g.zip _CATVER^|^|^(echo "%%g";"%%h"^)^>^>_NOTFOUND.csv)>>"output\%_file%.bat"
 
 )
 
-(echo title FINISHED
-echo pause^&exit)>>catver_ORGANIZER.bat
+title FINISHED
+echo ALL DONE, THE SCRIPT ITS IN THE OUTPUT FOLDER, RUN IT IN THE ROM FOLDER
+pause&exit
+REM exit /b
 
-cls
-echo              **** ALL DONE ***
-echo ---------------------------------------------------
-echo *   catver_ORGANIZER.bat was created, drag and drop *
-echo *  the rom folder to that script                   *
-echo *                                                  *
-echo --------------------------------------------------
-echo[
+
+:preview_list
+cls&echo ************ current list ******************
+echo.
+type _temp\found.tmp
+echo.
+REM pause & goto go_back
+pause&goto go_back
+
+
+
+
+
+
+rem //label
+:edit_list
+
+copy /y _temp\found.tmp _temp\temp.tmp
+
+:go_back2
+cls&echo ************ current list ******************
+echo.
+type _temp\temp.tmp
+echo.
+echo --------- ---Options --------------------
+echo 1. Save list and go back
+echo 2. Dissmis and go back
+echo.
+set /p "_opt3=Enter category remove or option: " || set _opt3=3
+
+findstr /irv /c:"=.*%_opt3%" _temp\temp.tmp >_temp\temp1.tmp
+
+del _temp\temp.tmp&ren _temp\temp1.tmp temp.tmp
+
+if "%_opt3%"=="1" (
+	del _temp\found.tmp&ren _temp\temp.tmp found.tmp
+	goto go_back
+)
+if "%_opt3%"=="2" del _temp\temp.tmp&goto go_back
+
+goto go_back2
+
+
+
+:build_romof
+rem //build romof bios, only parents have romof bios
+findstr /l /c:"romof=" "%_dat%" >_temp\temp.tmp
+for /f "delims=" %%g in (_temp\bios.tmp) do (
+	for /f tokens^=2^,%_src%^delims^=^" %%h in ('findstr /l /c:"romof=""%%g""" _temp\temp.tmp') do (
+		(echo %%h	%%i)>>_temp\romof.tmp
+	
+	)
+)
+exit /b
+
+
+:use_mamediff
+
+md _sources 2>nul
+for %%g in (_sources\*.dat) do set "_mame=%%g"
+for %%g in (_sources\*.ini) do set "_catver=%%g"
+
+if "%_mame%"=="" title ERROR&echo NO DATAFILE FOUND IN _SOURCES&pause&exit
+if "%_catver%"=="" title ERROR&echo CATVER.INI WAS NOT FOUND IN _SOURCES&pause&exit
+
+if exist "mamediff.exe" set "_mamediff=mamediff"
+if exist "_bin\mamediff.exe" set "_mamediff=_bin\mamediff"
+if "%_mamediff%"=="" title ERROR&echo MAMEDIFF.EXE WAS NOT FOUND&pause&exit
+
+%_mamediff% -s "%_mame%" "%_dat%" >nul
+move mamediff.out "_temp\%_file%.tmp"&del mamediff.log
+
+rem //rebuild catver.ini with mamediff.out
+for /f "tokens=1,2 delims=	" %%g in ('findstr /xr /c:"[a-z0-9_][a-z0-9_]*	[a-z0-9_][a-z0-9_]*" "_temp\%_file%.tmp"') do (
+	echo %%g
+
+	for /f "tokens=2 delims==" %%i in ('findstr /xr /c:"%%g=[^0][^.]..*" "%_catver%"') do (
+		(echo %%h=%%i)>>_temp\catver.tmp
+		(echo %%i)>>_temp\temp.tmp
+	)
+)
+
 
 exit /b
 
 
+:no_mamediff
 
-:dups_remover
-
-REM ****** line counter ************
-set /a "_total_lines=0" & set /a "_count_lines=0"
-for /f "delims=" %%g in (_temp\%1) do set /a "_total_lines+=1"
-
-type nul >_temp\nodups.1 2>nul
-
-for /f "delims=" %%g in (_temp\%1) do (
-	set /a "_con=0"
-	
-	for /f "delims=" %%h in (_temp\nodups.1) do if "%%g"=="%%h" set /a "_con+=1"
-	
-	REM ****** line counter ************	
-	set /a "_count_lines+=1"
-	set /a "_percent=(!_count_lines!*100)/!_total_lines!
-	title Building Menu: !_count_lines! / !_total_lines! ^( !_percent! %% ^)
-
-	if "!_con!"=="0" (echo %%g) >>_temp\nodups.1
+rem //get gamelist from datafile
+findstr /l /c:"<game name=" "%_dat%" >"_temp\%_file%.tmp"
+if %errorlevel% equ 1 (
+	findstr /l /c:"<machine name=" "%_dat%" >"_temp\%_file%.tmp"
 
 )
 
-del _temp\%1 & ren _temp\nodups.1 %1
+
+for /f usebackq^tokens^=2^delims^=^" %%g in ("_temp\%_file%.tmp") do (
+	echo %%g
+	for /f "tokens=2 delims==" %%i in ('findstr /xr /c:"%%g=[^0][^.]..*" "%_catver%"') do (
+		(echo %%g=%%i)>>_temp\catver.tmp
+		(echo %%i)>>_temp\temp.tmp
+	)
+)
+
+
+REM findstr /xr /c:".[a-z0-9_]*=[^0][^.]..*" "%_catver%" >_temp\catver.tmp
+REM for /f "tokens=2 delims==" %%i in (_temp\catver.tmp) do (
+	REM echo %%i
+	REM (echo %%i)>>_temp\temp.tmp
+REM )
+
 
 exit /b
